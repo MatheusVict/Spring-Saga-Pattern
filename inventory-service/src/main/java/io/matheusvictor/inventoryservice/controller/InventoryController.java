d162ab824c8ac9fb14e749a2b65f5ba76c90bc93
MatheusVict <matheusvictorhenrique@gmail.com>
2024-05-19 20:30:44 -0300

    fix: add constructor
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- inventory-service/src/main/java/io/matheusvictor/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/io/matheusvictor/inventoryservice/controller/InventoryController.java
@@ -3,8 +3,6 @@ package io.matheusvictor.inventoryservice.controller;
 import io.matheusvictor.inventoryservice.dto.InventoryRequestDTO;
 import io.matheusvictor.inventoryservice.dto.InventoryResponseDTO;
 import io.matheusvictor.inventoryservice.service.InventoryService;
-import io.matheusvictor.inventoryservice.service.impl.InventoryServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/inventory")
 public class InventoryController {
 
-    @Autowired
-    private InventoryService inventoryService;
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
 
     @PostMapping("/deduct")
     public ResponseEntity<InventoryResponseDTO> deduct(@RequestBody InventoryRequestDTO body) {
